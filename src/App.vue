c1c90a6363f46be339f5e088ad79aeaf48feadff
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2022-01-28 11:40:54 -0500

    Created app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button :disabled="!locationEnabled" style="margin-bottom: 10px">
+      I am enabled when location is enabled
+    </button>
+    <div v-show="locationEnabled">
+      latitude: {{ latitude }} longitude: {{ longitude }}
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { reactive, toRefs } from "vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  setup() {
+    const state = reactive({
+      locationEnabled: false,
+      latitude: undefined,
+      longitude: undefined,
+    });
+    const checkForLocationPermissions = () => {
+      if (!navigator.geolocation) {
+        state.locationEnabled = false;
+        return;
+      }
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then((result) => {
+          if (result.state === "granted") {
+            state.locationEnabled = true;
+            navigator.geolocation.watchPosition((pos) => {
+              console.log("position watcher");
+              const { coords } = pos;
+              state.longitude = coords.longitude;
+              state.latitude = coords.latitude;
+            });
+          } else if (result.state === "denied") {
+            state.locationEnabled = false;
+          } else if (result.state === "prompt") {
+            navigator.geolocation.getCurrentPosition(() => {});
+          }
+          result.onchange = () => {
+            result.state === "granted"
+              ? (state.locationEnabled = true)
+              : (state.locationEnabled = false);
+          };
+        })
+        .catch((error) => {
+          console.log("Erro: ", error);
+        });
+    };
+
+    checkForLocationPermissions();
+    return { ...toRefs(state) };
+  },
+};
 </script>
 
 <style>
